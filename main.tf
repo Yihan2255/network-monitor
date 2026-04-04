41879154521fba02be0ad3a34ca94c2609c45ce2
Yihan2255 <yihan2255@gmail.com>
2026-04-04 11:09:53 +0200

    Step 3: Added SNS Topic and Email Subscription

--- main.tf
+++ main.tf
@@ -10,3 +10,16 @@ terraform {
 provider "aws" {
     region = "eu-south-2"
 }
+
+# 1. Create the "Topic" (the Alarm Channel)
+resource "aws_sns_topic" "website_health_alerts" {
+    name = "website_health_alerts"
+}
+
+# 2. Create the "Subscription" (Your Email)
+resource "aws_sns_topic_subscription" "email_alert" {
+    topic_arn = aws_sns_topic.website_health_alerts.arn
+    protocol = "email"
+    endpoint = "yihan2255@gmail.com"
+}
+
